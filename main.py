d7c775c5440c9a752d65117e985aa00ebb606fb3
Shane McDonough <shanemcdman@gmail.com>
2021-04-05 01:55:32 -0700

    Get basic truth tables working

--- main.py
+++ main.py
@@ -18,21 +18,27 @@ class Token:
     @staticmethod
     def read_string(string: str) -> ['Token', ...]:
         result = []
+        prev_was_alpha = False
         for ch in string:
             if ch.isalpha():
+                if prev_was_alpha:
+                    result.append(Token(TokenType.OP_AND))
                 result.append(Token(TokenType.VARIABLE, ch))
-            elif ch == '(':
-                result.append(Token(TokenType.L_PAREN))
-            elif ch == ')':
-                result.append(Token(TokenType.R_PAREN))
-            elif ch in ['|', '+']:
-                result.append(Token(TokenType.OP_OR))
-            elif ch == '^':
-                result.append(Token(TokenType.OP_XOR))
-            elif ch in ['.', '', '&']:
-                result.append(Token(TokenType.OP_AND))
-            elif ch in ['~', '!']:
-                result.append(Token(TokenType.OP_NOT))
+                prev_was_alpha = True
+            else:
+                prev_was_alpha = False
+                if ch == '(':
+                    result.append(Token(TokenType.L_PAREN))
+                elif ch == ')':
+                    result.append(Token(TokenType.R_PAREN))
+                elif ch in ['|', '+']:
+                    result.append(Token(TokenType.OP_OR))
+                elif ch == '^':
+                    result.append(Token(TokenType.OP_XOR))
+                elif ch in ['.', '', '&']:
+                    result.append(Token(TokenType.OP_AND))
+                elif ch in ['~', '!']:
+                    result.append(Token(TokenType.OP_NOT))
         return result
 
 @dataclass
@@ -43,7 +49,7 @@ class Variable:
         return env[self.name]
 
 class BinaryOperator:
-    func = lambda a, b: 0
+    func = lambda self, a, b: 0
 
     def __init__(self, left, right):
         self.left = left
@@ -53,48 +59,61 @@ class BinaryOperator:
         return self.func(self.left.exec(env), self.right.exec(env))
 
 class Or(BinaryOperator):
-    func = lambda a, b: a | b
+    func = lambda self, a, b: a | b
 
 class Xor(BinaryOperator):
-    func = lambda a, b: a ^ b
+    func = lambda self, a, b: a ^ b
 
 class And(BinaryOperator):
-    func = lambda a, b: a & b
+    func = lambda self, a, b: a & b
 
 class Not:
     def __init__(self, var):
         self.var = var
-        self.func = lambda a: ~a
+        self.func = lambda a: (a + 1) % 2
 
     def exec(self, env: dict) -> int:
         return self.func(self.var.exec(env))
 
-def nloops(n: int, to_loop: 'iterable', func = lambda x: 0,*args):
+def create_equation(token_list: [Token, ...]):
+    # algo to impliment
+    # find main operator
+    # recurse on other parts if needed.
+    if len(token_list) == 1:
+        return Variable(token_list[0].value)
+    for i, item in enumerate(token_list):
+        if item.token_type is TokenType.OP_AND:
+            return And(create_equation(token_list[i - 1: i]), create_equation(token_list[i + 1: i + 2]))
+        elif item.token_type is TokenType.OP_XOR:
+            return Xor(create_equation(token_list[i - 1: i]), create_equation(token_list[i + 1: i + 2]))
+        elif item.token_type is TokenType.OP_OR:
+            return Or(create_equation(token_list[i - 1: i]), create_equation(token_list[i + 1: i + 2]))
+        elif item.token_type is TokenType.OP_NOT:
+            return Not(create_equation(token_list[i + 1: i + 2]))
+
+def nloops(n: int, to_loop: 'iterable', func, lst, *args):
     if n == 0:
-        print(*args, func(args), sep = ' | ')
+        dct = {}
+        for i, item in enumerate(lst):
+            dct[lst[i]] = args[i]
+        print(*args, func(dct), sep = ' | ')
         return
     for i in to_loop:
-        nloops(n - 1, to_loop, func, *args, i)
+        nloops(n - 1, to_loop, func, lst, *args, i)
 
-def print_truth_table(eq: str):
-    ops = {
-            '^': lambda a, b: a ^ b,
-            '|': lambda a, b: a | b,
-            '&': lambda a, b: a & b,
-            '.': lambda a, b: a & b,
-            '': lambda a, b: a & b,
-            }
-    items = list({ch for ch in eq if ch.isalpha()})
-    print(*items, eq, sep = ' | ')
-    op_char = eq
-    for ch in eq:
-        if ch.isalpha():
-            op_char = op_char.replace(ch, '')
-    op = ops[op_char]
-    func = lambda args: op(args[0], args[1])
-    nloops(len(items), range(0, 2), func)
+def print_truth_table(eq_str: str):
+    items = list({ch for ch in eq_str if ch.isalpha()})
+    print(*items, eq_str, sep = ' | ')
+    l = Token.read_string(eq_str)
+    eq = create_equation(l)
+    nloops(len(items), range(0, 2), lambda x: eq.exec(x), items)
 
 if __name__ == '__main__':
-    # print_truth_table('AB')
-    l = Token.read_string('A^B')
-    print(l)
+    print_truth_table('AB')
+    print()
+    print_truth_table('A|B')
+    print()
+    print_truth_table('A^B')
+    print()
+    print_truth_table('!A')
+    print()
